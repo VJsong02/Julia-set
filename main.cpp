33cdb7150de6f740840cb5dbfa9c1b40c68d1e94
Victor Song <VJsong02@users.noreply.github.com>
2019-08-15 03:21:33 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,43 +1,42 @@
 #include "CImg.h" // http://cimg.eu/
+#include <chrono>
 #include <iostream>
 #include <omp.h>
-#include <ctime>
+#include <sstream>
+
 using namespace std;
 
-int main() {
-	cout << "Resolution: ";
-	int a;
-	cin >> a; cin.ignore();
-	double cx, cy;
-	cout << "c (real): ";
-	cin >> cx; cin.ignore();
-	cout << "c (imaginary): ";
-	cin >> cy; cin.ignore();
-	cout << "Iterations: ";
-	unsigned short iterations;
-	cin >> iterations; cin.ignore();
+int main(int argc, char *argv[]) {
+	int a = stoi(argv[1]) / 2, b = stoi(argv[2]) / 2;
+	double cx = stod(argv[3]), cy = stod(argv[4]);
+	unsigned short iterations = stoi(argv[5]);
 
-	cimg_library::CImg<unsigned short> julia(a * 2, a, 1, 1);
+	cout << a * 2 << "x" << b * 2<< " " << argv[3] << " " << argv[4] << " " << argv[5] << endl;
 
-	float start = static_cast<float>(clock());
+	cimg_library::CImg<unsigned short> julia(a, b, 1, 1);
+
+	chrono::high_resolution_clock::time_point start = chrono::high_resolution_clock::now();
 	omp_set_num_threads(omp_get_num_procs());
 #pragma omp parallel for schedule(dynamic)
-	for (int y = -(a / 2); y < (a / 2); y += 1) {
+	for (int y = -b; y < b; y += 1) {
 		for (int x = -a; x < a; x += 1) {
-			double zx = -(x / (double)(a / 2)), zy = y / (double)(a / 2);
+			double zx = -(x / (double)a), zy = y / (double)b;
 			double xtemp;
 
 			unsigned short i = 0;
 			for (i = 0; i < iterations && zx * zx - zy * zy < 4; i += 1) {
-				xtemp = zx * zx - zy * zy;
+				zx = zx * zx - zy * zy + cx;
 				zy = 2 * zx * zy + cy;
-				zx = xtemp + cx;
 			}
-			julia(x + a, y + (a / 2)) = i;
+			julia(x + a, y + b) = i;
 		}
 	}
-	cout << (static_cast<float>(clock()) - start) / 1000 << " s" << endl;
-	if (MessageBox(NULL, "Would you like to save the image?", "Julia", MB_YESNO) == 6)
-		julia.save_png("D:\julia.png");
-	julia.display();
+	cout << chrono::duration_cast<chrono::duration<double>>(chrono::high_resolution_clock::now() - start).count() << " s" << endl;
+
+	ostringstream ss;
+	ss << "./" << a << "x" << b << " " << argv[3] << " " << argv[4] << " " << argv[5] << ".png";
+	string filename = ss.str();
+	julia.save_png(filename.c_str());
+
+	return 0;
 }
